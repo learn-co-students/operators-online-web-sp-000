04bf27f7beda606aeef9b947102b68e6f9813696
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 01:26:24 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
-def unsafe?(speed)
+require 'pry'
 
+
+def unsafe?(speed)
+  speed > 60 || speed < 40 ? true : false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
