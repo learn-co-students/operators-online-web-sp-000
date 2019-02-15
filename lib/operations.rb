b5100d3ee7259187e5c8c1ef4ea54253b2aa07ba
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-15 20:55:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,23 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60 ? true : false
+	  return true
+	elsif speed < 40 ? true : false
+	  return true
+	else
+	  return false
+	end
 end
 	
 
