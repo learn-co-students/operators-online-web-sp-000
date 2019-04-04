2c0051dbf354e39ad56b13465b669936c21f68b0
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-04 02:30:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,12 +3,13 @@ def unsafe?(speed)
     true 
   else 
     false 
+  end 
 end
 
 
 
 def not_safe?(speed)
-	if speed < 40 || speed > 60 ? true : false 
+	speed < 40 || speed > 60 ? true : false 
 end
 	
 
