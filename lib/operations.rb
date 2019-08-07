0b787ce08f21a3c4bfa9e70279950514d2083cdf
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-07 00:45:35 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+if speed > 60
+  return TRUE
+elsif speed < 60
+return false
+else speed > 40
+  return false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed ? < 60 : 40
+	return false
 end
 	
 
