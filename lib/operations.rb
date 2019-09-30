5595162afd76640589735ceb126975a10045616b
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-30 18:42:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
-
+if speed < 40 || speed > 60 
+  return true 
+elsif speed.between?(40,60)
+return false 
+else 
 end
-
+end 
 
 
 def not_safe?(speed)
-	
+	if speed > 60 || speed < 40
+	  return true 
+	 elsif speed.between?(40,60)
+	 return false 
+	 else
+	 end 
 end
 	
 
