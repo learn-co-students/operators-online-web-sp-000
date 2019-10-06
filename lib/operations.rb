9f667830dbfb2abce7d7ee3dadc0210231f353a3
Anthony Tilelli <anthony@tilelli.me>
2019-10-06 19:43:36 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if 40 < speed && speed < 60
+    return FALSE
+  else
+    return TRUE
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	40 < speed && speed < 60 ?  FALSE : TRUE
 end
 	
 
