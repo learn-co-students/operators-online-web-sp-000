80871a61ff2102c52e6b800ec5c2cc9502e3789e
Will Corcoran <corcoranwill@gmail.com>
2020-02-15 12:24:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
+MIN_SAFE_SPEED = 40
+MAX_SAFE_SPEED = 60
 
+def unsafe?(speed)
+	if speed < MIN_SAFE_SPEED || speed > MAX_SAFE_SPEED
+		true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < MIN_SAFE_SPEED || speed > MAX_SAFE_SPEED  ? true : false
 end
-	
-
-
