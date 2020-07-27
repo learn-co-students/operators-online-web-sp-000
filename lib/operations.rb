0d60a837fa7e5d14ab2c0ce7ad06a188935d5d86
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-27 15:11:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+ if speed < 40 ; true
+ elsif speed > 60 ;true
+ 	else false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+ speed > 60 ? true : false
+ speed < 40 ? true : false
+ speed.between?(40, 60) ? false : true
 end
-	
-
-
