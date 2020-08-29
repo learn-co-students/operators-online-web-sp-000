45ac925966dbcea23c363420a4727bae8002e566
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-29 20:38:15 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  #ternary version: (speed < 40 || speed > 60) ? true : false
+if speed < 40 || speed > 60
+  true
+else false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+(speed > 60 || speed < 40) ? true : false
 end
-	
-
-
