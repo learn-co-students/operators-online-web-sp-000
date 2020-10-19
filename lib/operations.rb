26c04dd0e2510b5e27c3ee8fadef1ae0d44e9ec4
VandelayUtd <drewhadley.guitar@gmail.com>
2020-10-19 12:08:17 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,16 @@
 def unsafe?(speed)
+    if speed > 60 || speed < 40
+        true
+    else
+        false
+    end
 
 end
 
 
 
 def not_safe?(speed)
+    !speed.between?(41, 59) ? true :false
 	
 end
 	
