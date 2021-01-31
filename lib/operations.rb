0f3a555d6b274b48c2ba1f4cc9e402412781c359
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-01-31 21:20:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
 
+
+def unsafe?(speed)
+if      speed < 40 || 60 < speed
+ return true
+else
+	return false
+end
 end
 
 
 
 def not_safe?(speed)
+	speed < 40 || speed > 60 ? true : false
 	
 end
-	
-
-
