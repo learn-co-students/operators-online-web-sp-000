5167079691d438af3af2c37995dcaed27a399ee3
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-22 20:58:32 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+if speed > 60
+  true
+elsif speed < 40
+  true
+else
+  false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60
+    true
+  elsif speed < 40 ? true : false
+    true
+  else
+    false
+    end
 end
-	
-
-
