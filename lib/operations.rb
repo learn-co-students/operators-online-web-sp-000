252876601b153d4bd03b9c3457bd6c3a7bbb8927
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-01 17:53:48 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
+require 'pry'
 def unsafe?(speed)
-
+  if speed < 40 == true || speed > 60 == true 
+    return true
+  else 
+    return false
+  end 
 end
 
 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
 	
 
 
+puts unsafe?(30)
+puts unsafe?(50)
+puts unsafe?(60)
